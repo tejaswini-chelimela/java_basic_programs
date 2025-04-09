c548b8a16f6518cccde3af6f631cd672ddd17a68
tejaswini-chelimela <tejachelimela@gmail.com>
2025-04-09 16:01:07 +0530

    Add files via upload

---
+++ Details.java
@@ -0,0 +1,9 @@
+class Details{
+	public static void main(String[] args){
+		System.out.println("name = teja");
+		System.out.println("age = 20");
+		System.out.println("gender= female");
+		System.out.println("GmailId = tejachelimela12@gmail.com");
+		System.out.println("Height = 6.5");
+	}
+}
